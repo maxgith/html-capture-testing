ad6f365f2ad7e338b788383f222ac079060d821e
Maxim Baranov <maxpostbox1@gmail.com>
2024-11-14 10:31:19 +0100

    Init server

---
+++ server.mjs
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = 8080;
+
+app.use(express.static('.'))
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
